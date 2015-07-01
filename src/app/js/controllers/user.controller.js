f7e1bc1980d107baa2c3b485687fbf6937e04436
rz <rz@nemetos.net>
2015-07-01 12:59:58 +0300

    user page

--- src/app/js/controllers/user.controller.js
+++ src/app/js/controllers/user.controller.js
@@ -4,10 +4,44 @@
   angular.module('gitParser')
     .controller('UserController', UserController);
 
-  function UserController($http, $stateParams, $log, $scope) {
+  function UserController($http, $stateParams, $log, $scope, headerParser) {
+
+    $scope.currentPage = 1;
+    $scope.repos = [];
+    $scope.perPage = 10;
+    $scope.showPages = function () {
+      return $scope.perPage < $scope.totalItems;
+    };
+
+
     $http({method: 'GET', url: 'https://api.github.com/users/' + $stateParams.login})
       .success(function (data) {
         $scope.user = data;
+        $scope.reposHost = data.repos_url;
+        $http({method: 'GET', url: data.repos_url + '?page=1&per_page=1'})
+          .success(function (data, status, headers) {
+            if (headers('Link')) {
+              $scope.totalItems = headerParser(headers('Link')).last;
+            }
+            function getData() {
+              $http.get($scope.reposHost + '?page=' + $scope.currentPage + '&per_page=' + $scope.perPage)
+                .then(function (response) {
+                  angular.copy(response.data, $scope.repos)
+                });
+            }
+
+            $scope.pageChanged = function () {
+              getData();
+            };
+            getData();
+          })
       });
+
+
+
+
+
+
+
   }
 })();
